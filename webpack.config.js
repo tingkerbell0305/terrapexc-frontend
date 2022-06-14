5392214070e312cd7756002d539429d431f0d886
tingkerbell0305 <babydragon0305@gmail.com>
2022-06-15 00:00:00 +0900

    "2022-06-15 modified"

--- webpack.config.js
+++ webpack.config.js
@@ -8,17 +8,6 @@ module.exports = (config, context) => {
       fallback: {
         'process/browser': require.resolve('process/browser')
       }
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          enforce: 'pre',
-          use: [
-            "style-loader",
-            "css-loader",
-            {
-              loader: "sass-loader",
               options: {
                 // Prefer `dart-sass`
                 implementation: require("sass"),
